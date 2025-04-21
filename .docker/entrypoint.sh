dc9d5fc0f9ee1d119e296c3fd018334df0ff4867
Vojtěch Kaizr <studio@studio83.cz>
2025-04-21 09:49:36 +0200

    fixed temp, log and www permissions

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,9 +2,12 @@
 set -e
 
 mkdir -p /srv/www/webtemp
-mkdir -p /srv/temp
 mkdir -p /srv/log
+mkdir -p /srv/temp
 chown -R www-data:www-data /srv/www
+chmod -R 755 /srv/www
+chmod -R 777 /srv/log
+chmod -R 777 /srv/temp
 
 cd /srv
 composer install
